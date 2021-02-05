93b3a4119f27945434be23d00a03fec62fe50395
Julio Cesar <rasjuliocesar@gmail.com>
2021-02-05 19:25:45 -0300

    Project HerokuCenterAPI Created

---
+++ src/main/java/com/herokuCenterApi/HerokuCenterApiApplication.java
@@ -0,0 +1,13 @@
+package com.herokuCenterApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuCenterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuCenterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/herokuCenterApi/HerokuCenterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herokuCenterApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuCenterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
