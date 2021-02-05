c65ff653495f5b9c3bc62fd6abaa89297bd35e85
Julio Cesar <rasjuliocesar@gmail.com>
2021-02-05 20:03:11 -0300

    Annotations Restriction

--- src/main/java/com/herokuCenterApi/entities/Restriction.java
+++ src/main/java/com/herokuCenterApi/entities/Restriction.java
@@ -2,11 +2,22 @@ package com.herokuCenterApi.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.sun.istack.NotNull;
+
+@Entity
 public class Restriction implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@NotNull
 	private String documentNumber;
 	
 	public Restriction() {
