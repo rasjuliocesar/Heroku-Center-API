bb2781eeba8e13bf35865650f38389e345b9cb2c
Julio Cesar <rasjuliocesar@gmail.com>
2021-02-05 20:39:47 -0300

    Annotations and Methods - RestrictionResource and RestrictionService class

--- src/main/java/com/herokuCenterApi/resources/RestrictionResource.java
+++ src/main/java/com/herokuCenterApi/resources/RestrictionResource.java
@@ -1,19 +1,33 @@
 package com.herokuCenterApi.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.herokuCenterApi.entities.Restriction;
+import com.herokuCenterApi.services.RestrictionService;
 
 @RestController
 @RequestMapping(value = "/restriction")
 public class RestrictionResource {
 
+	@Autowired
+	private RestrictionService service;
+	
 	@GetMapping
-	public ResponseEntity<Restriction> findAll(){
-		Restriction rest = new Restriction(1L, "0000000000");
+	public ResponseEntity<List<Restriction>> findAll(){
+		List<Restriction> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Restriction> findById(@PathVariable Long id){
+		Restriction rest = service.findById(id);
 		return ResponseEntity.ok().body(rest);
 	}
 }
--- src/main/java/com/herokuCenterApi/services/RestrictionService.java
+++ src/main/java/com/herokuCenterApi/services/RestrictionService.java
@@ -1,12 +1,15 @@
 package com.herokuCenterApi.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.herokuCenterApi.entities.Restriction;
 import com.herokuCenterApi.repositories.RestrictionRepository;
 
+@Service
 public class RestrictionService {
 
 	@Autowired
@@ -14,5 +17,10 @@ public class RestrictionService {
 	
 	public List<Restriction> findAll(){
 		return repository.findAll();
+	}
+	
+	public Restriction findById(Long id) {
+		Optional<Restriction> opt = repository.findById(id);
+		return opt.get();
 	}
 }
