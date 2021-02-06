22ec422ffdd1bd5b819f1560cb3e2cf773c35ac5
Julio Cesar <rasjuliocesar@gmail.com>
2021-02-06 18:44:35 -0300

    Create DatabaseException - Method databaseException (ResourceExceptionHandler class) - Try_Catch method delete (RestrictionService)

--- src/main/java/com/herokuCenterApi/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/herokuCenterApi/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.herokuCenterApi.services.exceptions.DatabaseException;
 import com.herokuCenterApi.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -22,5 +23,12 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(status).body(err);
 	}
 	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> databaseException(ResourceNotFoundException rnfe, HttpServletRequest request){
+		String error = "Database Error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, rnfe.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
 	
 }
