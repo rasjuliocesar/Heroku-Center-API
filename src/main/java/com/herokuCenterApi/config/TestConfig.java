7f78ded87b6afb961778e76e91af515c1347dd10
Julio Cesar <rasjuliocesar@gmail.com>
2021-02-06 21:25:17 -0300

    Registration / RegistrationRepository class - created | TestConfig class include Registration test

--- src/main/java/com/herokuCenterApi/config/TestConfig.java
+++ src/main/java/com/herokuCenterApi/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.herokuCenterApi.config;
 
+import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.util.Arrays;
 
@@ -8,7 +9,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.herokuCenterApi.entities.Registration;
 import com.herokuCenterApi.entities.Restriction;
+import com.herokuCenterApi.repositories.RegistrationRepository;
 import com.herokuCenterApi.repositories.RestrictionRepository;
 
 @Configuration
@@ -17,15 +20,25 @@ public class TestConfig implements CommandLineRunner{
 
 	@Autowired
 	private RestrictionRepository restrictionRepository;
+	@Autowired
+	private RegistrationRepository registrationRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
 		Restriction rest1 = new Restriction(null, "00000000000", true, Instant.now(), Instant.now());
 		Restriction rest2 = new Restriction(null, "11111111111", true, Instant.now(), Instant.now());
 		Restriction rest3 = new Restriction(null, "22222222222", true, Instant.now(), Instant.now());
 		Restriction rest4 = new Restriction(null, "33333333333", true, Instant.now(), Instant.now());
 		
 		restrictionRepository.saveAll(Arrays.asList(rest1, rest2, rest3, rest4));
+		
+		Registration reg = new Registration(1L, "Um Dois Três de Oliveira Quatro", "Boa Viagem Avenue", "51513220", "Pernambuco", "Brazil", 
+				sdf.parse("01/08/1986"), "12345678910", "+5581912345678", "1234@email.com", "APPROVED", 104);
+		
+		registrationRepository.saveAll(Arrays.asList(reg));
 	}
 	
 	
---
+++ src/main/java/com/herokuCenterApi/entities/Registration.java
@@ -0,0 +1,182 @@
+package com.herokuCenterApi.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.sun.istack.NotNull;
+
+@Entity
+public class Registration implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotNull
+	private String fullName;
+	@NotNull
+	private String address;
+	@NotNull
+	private String zipCode;
+	@NotNull
+	private String state;
+	@NotNull
+	private String country;
+	@NotNull
+	private Date birthDate;
+	@NotNull
+	private String document;
+	@NotNull
+	private String phone;
+	@NotNull
+	private String email;
+	private String decision;
+	private Integer bankCode;
+
+	public Registration() {
+	}
+
+	public Registration(Long id, String fullName, String address, String zipCode, String state, String country,
+			Date birthDate, String document, String phone, String email, String decision, Integer bankCode) {
+		super();
+		this.id = id;
+		this.fullName = fullName;
+		this.address = address;
+		this.zipCode = zipCode;
+		this.state = state;
+		this.country = country;
+		this.birthDate = birthDate;
+		this.document = document;
+		this.phone = phone;
+		this.email = email;
+		this.decision = decision;
+		this.bankCode = bankCode;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getDocument() {
+		return document;
+	}
+
+	public void setDocument(String document) {
+		this.document = document;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDecision() {
+		return decision;
+	}
+
+	public void setDecision(String decision) {
+		this.decision = decision;
+	}
+
+	public Integer getBankCode() {
+		return bankCode;
+	}
+
+	public void setBankCode(Integer bankCode) {
+		this.bankCode = bankCode;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Registration other = (Registration) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
